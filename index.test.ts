0253b06dc6221e4812f8eaadc19231cc68768311
rainy-me <github@rainy.me>
2020-11-23 04:13:35 +0900

    add delay and ttl

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.78.0/testing/asserts.ts";
+import { delay } from "https://deno.land/std@0.78.0/async/delay.ts";
 import { connect } from "https://deno.land/x/redis/mod.ts";
 
 const redis = await connect({
@@ -7,8 +8,24 @@ const redis = await connect({
 });
 
 Deno.test("get set", async () => {
-  const ok = await redis.set("k", "v");
-  assertEquals(ok, "OK");
-  const v = await redis.get("k");
-  assertEquals(v, "v");
+  assertEquals(await redis.set("k", "v"), "OK");
+  assertEquals(await redis.get("k"), "v");
+});
+
+Deno.test("mget mset", async () => {
+  assertEquals(await redis.mset({ a: "a", b: "b" }), "OK");
+  assertEquals(await redis.mget("a", "b"), ["a", "b"]);
+});
+
+Deno.test("delay and ttl", async () => {
+  await redis.set("delayK", "delayV");
+  assertEquals(await redis.ttl("delayK"), -1);
+  assertEquals(await redis.expire("delayK", 2), 1);
+  assertEquals(await redis.get("delayK"), "delayV");
+  assertEquals((await redis.ttl("delayK")) > 0, true);
+  await delay(2000);
+
+  assertEquals(await redis.get("delayK"), undefined);
+  assertEquals(await redis.ttl("delayK"), -2);
+  assertEquals(await redis.expire("delayK", 2), 0);
 });
